cf0a6744726f974d83dac9dc61434a057aed4872
Owen Stanley <owentgstanley@gmail.com>
2024-12-04 21:28:44 +0000

    adding leafnode class, adding tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from textnode import TextNode, TextType
+from leafnode import LeafNode
 
 def main():
     t1 = TextNode("this is a bold node", TextType.BOLD, "https://www.boot.dev")
+    l1 = LeafNode("p", "This is a paragraph of text.")
+    l2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
+    print(l1.to_html())
+    print(l2.to_html())
 
 if __name__ == "__main__":
     main()
---
+++ src/test_leafnode.py
@@ -0,0 +1,11 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
